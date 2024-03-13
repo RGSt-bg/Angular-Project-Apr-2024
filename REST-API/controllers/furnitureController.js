e82fdb751fcc319b4f81982e6d5dfa445a11c066
Rumen Stefanov <rgst.bg@gmail.com>
2024-03-13 22:09:24 +0200

    Created search functionality

--- REST-API/controllers/furnitureController.js
+++ REST-API/controllers/furnitureController.js
@@ -1,153 +1,165 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const furnitureService = require('../services/furnitureService');
-const { isAuth } = require('../middlewares/authMiddleware');
-const { isProductOwner } = require('../middlewares/userMiddleware');
-const { getErrorMessage } = require('../utils/errorUtil');
-const Category = require('../models/Category');
-const Furniture = require('../models/Furniture');
-const { query } = require('express');
+const furnitureService = require("../services/furnitureService");
+const { isAuth } = require("../middlewares/authMiddleware");
+const { isProductOwner } = require("../middlewares/userMiddleware");
+const { getErrorMessage } = require("../utils/errorUtil");
+const Category = require("../models/Category");
+const Furniture = require("../models/Furniture");
+const { query } = require("express");
 
-router.get('/furnitures', async (req, res) => {
-    const furniture = await furnitureService.getAll().lean();
-    res.render('furniture/furnitures', { furniture });
+router.get("/furnitures", async (req, res) => {
+  const furniture = await furnitureService.getAll().lean();
+  res.render("furniture/furnitures", { furniture });
 });
 
-router.get('/furnitureList', async (req, res) => {
-    const furniture = await furnitureService.getAll().lean();
-    const calledFrom = req.query.calledFrom || '';
+router.get("/furnitureList", async (req, res) => {
+  const furniture = await furnitureService.getAll().lean();
+  const calledFrom = req.query.calledFrom || "";
 
-    let isNewTitle = '';
-    let isNew = '';
+  let isNewTitle = "";
+  let isNew = "";
 
-    if (calledFrom === 'newProducts') {
-        isNew = 'new';
-        isNewTitle = 'New';
-    }
-    else {
-        isNew = '';
-        isNewTitle = 'Our';
-    }
-        res.render('furniture/furnitureList', { furniture, isNewTitle, isNew });
+  if (calledFrom === "newProducts") {
+    isNew =
+      "Here you can find our NEW furnitures - elegant, comfortable, functional ...";
+    isNewTitle = "New";
+  } else if (calledFrom === "search") {
+    isNew = "The furniture that meets your criteria ...";
+    isNewTitle = "Found";
+  } else {
+    isNew =
+      "Here you can find our furnitures - elegant, comfortable, functional ...";
+    isNewTitle = "Our";
+  }
+  res.render("furniture/furnitureList", { furniture, isNewTitle, isNew });
 });
 
-router.get('/createCategory', async (req, res) => {
-    res.render('furniture/createCategory');
+router.get("/createCategory", async (req, res) => {
+  res.render("furniture/createCategory");
 });
 
-router.post('/createCategory', async (req, res) => {
-
-    const newCategory = req.body;
-    try {
-        await furnitureService.createCategory(newCategory);
-        res.render('furniture/createCategory');
-
-    } catch (err) {
-        console.log(err.message);
-        res.render('furniture/createCategory', { error: getErrorMessage(err) });
-    }
+router.post("/createCategory", async (req, res) => {
+  const newCategory = req.body;
+  try {
+    await furnitureService.createCategory(newCategory);
+    res.render("furniture/createCategory");
+  } catch (err) {
+    console.log(err.message);
+    res.render("furniture/createCategory", { error: getErrorMessage(err) });
+  }
 });
 
-router.get('/create', async (req, res) => {
-    const category = await furnitureService.getAllCategories().lean();
-    res.render('furniture/editCreate', { category, actionType: 'Create' });
+router.get("/create", async (req, res) => {
+  const category = await furnitureService.getAllCategories().lean();
+  res.render("furniture/editCreate", { category, actionType: "Create" });
 });
 
-router.post('/create', isAuth, async (req, res) => {
-    const newFurniture = req.body;
-    const category = await furnitureService.getAllCategories().lean();
-    const actionType = 'Create';
+router.post("/create", isAuth, async (req, res) => {
+  const newFurniture = req.body;
+  const category = await furnitureService.getAllCategories().lean();
+  const actionType = "Create";
 
-    try {
-        await furnitureService.create(req.user._id, newFurniture, { actionType });
+  try {
+    await furnitureService.create(req.user._id, newFurniture, { actionType });
 
-        res.redirect('/furniture/furnitureList');
-
-    } catch (err) {
-        console.log(err.message);
-        res.render('furniture/editCreate', { ...newFurniture, category, actionType: 'Create', error: getErrorMessage(err) });
-    }
+    res.redirect("/furniture/furnitureList");
+  } catch (err) {
+    console.log(err.message);
+    res.render("furniture/editCreate", {
+      ...newFurniture,
+      category,
+      actionType: "Create",
+      error: getErrorMessage(err),
+    });
+  }
 });
 
-router.get('/details/:furnitureId', async (req, res) => {
-    const furnitureId = req.params.furnitureId;
-    // const isLiker = await isStoneLiker(req, res);
+router.get("/details/:furnitureId", async (req, res) => {
+  const furnitureId = req.params.furnitureId;
+  // const isLiker = await isStoneLiker(req, res);
 
-    let isLoggedIn = false;
-    if (req.cookies['auth']) isLoggedIn = true;
+  let isLoggedIn = false;
+  if (req.cookies["auth"]) isLoggedIn = true;
 
-    try {
-        const furniture = await furnitureService.getOneDetailed(furnitureId).lean();
-        const isOwner = furniture.owner?._id == req.user?._id;
+  try {
+    const furniture = await furnitureService.getOneDetailed(furnitureId).lean();
+    const isOwner = furniture.owner?._id == req.user?._id;
 
-        res.render('furniture/details', { furniture, isOwner, isLoggedIn });
-    }
-    catch (err) {
-        console.log(err.message);
-        res.render('furniture/furnitureList', { error: getErrorMessage(err) });
-    }
+    res.render("furniture/details", { furniture, isOwner, isLoggedIn });
+  } catch (err) {
+    console.log(err.message);
+    res.render("furniture/furnitureList", { error: getErrorMessage(err) });
+  }
 });
 
-router.get('/edit/:furnitureId', isAuth, isProductOwner, async (req, res) => {
+router.get("/edit/:furnitureId", isAuth, isProductOwner, async (req, res) => {
+  let category = await furnitureService.getAllCategories().lean();
+  const furnitureId = req.params.furnitureId;
+  let furniture;
 
-    let category = await furnitureService.getAllCategories().lean();
-    const furnitureId = req.params.furnitureId;
-    let furniture;
-    
-    let isLoggedIn = false;
-    if (req.cookies['auth']) isLoggedIn = true;
-    
-    try {
-        furniture = await furnitureService.getOne(furnitureId).lean();
-        category = await excludeCategory(furniture.category, category);
+  let isLoggedIn = false;
+  if (req.cookies["auth"]) isLoggedIn = true;
 
-        res.render('furniture/editCreate', { furniture, category, actionType: 'Edit' });
-    }
-    catch (err) {
-        console.log(err.message);
-        res.render('furniture/editCreate', { ...furniture, category, actionType: 'Edit', error: getErrorMessage(err) });
-    }
+  try {
+    furniture = await furnitureService.getOne(furnitureId).lean();
+    category = await excludeCategory(furniture.category, category);
+
+    res.render("furniture/editCreate", {
+      furniture,
+      category,
+      actionType: "Edit",
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.render("furniture/editCreate", {
+      ...furniture,
+      category,
+      actionType: "Edit",
+      error: getErrorMessage(err),
+    });
+  }
 });
 
-router.post('/edit/:furnitureId', isAuth, async (req, res) => {
-    const editedFurniture = req.body;
+router.post("/edit/:furnitureId", isAuth, async (req, res) => {
+  const editedFurniture = req.body;
 
-    try {
-        await furnitureService.edit(req.params.furnitureId, editedFurniture);
-        res.redirect(`/furniture/details/${req.params.furnitureId}`);
-    }
-    catch (err) {
-        res.render(`furniture/editCreate/${req.params.furnitureId}`, { ...editedFurniture, error: getErrorMessage(err) });
-    }
+  try {
+    await furnitureService.edit(req.params.furnitureId, editedFurniture);
+    res.redirect(`/furniture/details/${req.params.furnitureId}`);
+  } catch (err) {
+    res.render(`furniture/editCreate/${req.params.furnitureId}`, {
+      ...editedFurniture,
+      error: getErrorMessage(err),
+    });
+  }
 });
 
-router.get('/like/:furnitureId', async (req, res) => {
-
-    const stoneId = req.params.stoneId;
-    const isLiker = await isStoneLiker(req, res);
-
-    let isLoggedIn = false;
-    if (req.cookies['auth']) isLoggedIn = true;
-
-    try {
-        await stoneService.like(req.user._id, stoneId)
-        const stone = await stoneService.getOneDetailed(stoneId).lean();
-        const isOwner = stone.owner?._id == req.user?._id;
-
-        res.render('furniture/details', { stone, isOwner, isLoggedIn, isLiker });
-    }
-    catch (err) {
-        console.log(err.message);
-        res.render('/furniture/dashboard', { error: getErrorMessage(err) });
-    }
+router.get("/delete/:furnitureId", isProductOwner, async (req, res) => {
+  console.log("In delete route");
+  await furnitureService.delete(req.params.furnitureId);
+  console.log("After delete action");
+  res.redirect("/furniture/furnitureList");
 });
 
-router.get('/delete/:furnitureId', isProductOwner, async (req, res) => {
-console.log('In delete route');
-    await furnitureService.delete(req.params.furnitureId);
-console.log('After delete action');
-    res.redirect('/furniture/furnitureList');
-});
+// router.get("/like/:furnitureId", async (req, res) => {
+//   const stoneId = req.params.stoneId;
+//   const isLiker = await isStoneLiker(req, res);
+
+//   let isLoggedIn = false;
+//   if (req.cookies["auth"]) isLoggedIn = true;
+
+//   try {
+//     await stoneService.like(req.user._id, stoneId);
+//     const stone = await stoneService.getOneDetailed(stoneId).lean();
+//     const isOwner = stone.owner?._id == req.user?._id;
+
+//     res.render("furniture/details", { stone, isOwner, isLoggedIn, isLiker });
+//   } catch (err) {
+//     console.log(err.message);
+//     res.render("/furniture/dashboard", { error: getErrorMessage(err) });
+//   }
+// });
 
 // async function isStoneLiker(req, res) {
 //     const userId = req.user?._id;
@@ -161,24 +173,22 @@ console.log('After delete action');
 //     return true;
 // };
 
-router.get('/search', async (req, res) => {
+router.get("/search", async (req, res) => {
 
-    let stone, name;
-    const isLiker = await isStoneLiker(req, res);
-    
-    if (Object.keys(req.query).length === 0) {
-        stone = await stoneService.getAll().lean();
-    }
-    else {
-        const name = req.query;
-        stone = (await stoneService.search(name));
-    }
-
-    res.render('furniture/search', { stone, name, isLiker });
+  try {
+    const name = req.query.search;
+    const furniture = await furnitureService.search(name);
+    res.render("furniture/furnitureList", { furniture });
+  }
+  
+  catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
 });
 
 function excludeCategory(categoryName, category) {
-    return category.filter(furniture => furniture.category != categoryName);
+  return category.filter((furniture) => furniture.category != categoryName);
 }
 
 module.exports = router;
--- REST-API/services/furnitureService.js
+++ REST-API/services/furnitureService.js
@@ -1,39 +1,77 @@
-const User = require('../models/User');
-const Category = require('../models/Category');
-const Furniture = require('../models/Furniture');
+const User = require("../models/User");
+const Category = require("../models/Category");
+const Furniture = require("../models/Furniture");
 
 exports.getAll = () => Furniture.find();
 
 exports.getOne = (furnitureId) => Furniture.findById(furnitureId);
 
-exports.getOneDetailed = (furnitureId) => this.getOne(furnitureId).populate('owner');
+exports.getOneDetailed = (furnitureId) =>
+  this.getOne(furnitureId).populate("owner");
 
 exports.createCategory = async (categoryData) => {
-    const addedCategory = await Category.create(categoryData);
+  const addedCategory = await Category.create(categoryData);
 };
 
 exports.getAllCategories = () => Category.find();
 
 exports.create = async (userId, furnitureData) => {
-    const addedFurniture = await Furniture.create({
-        owner: userId,
-        ...furnitureData
-    });
+  const addedFurniture = await Furniture.create({
+    owner: userId,
+    ...furnitureData,
+  });
 
-    await User.findByIdAndUpdate(userId, { $push: { buyedList: addedFurniture._id } })
+  await User.findByIdAndUpdate(userId, {
+    $push: { buyedList: addedFurniture._id },
+  });
 };
 
-exports.like = async (userId, furnitureId) => {
-    await Furniture.findByIdAndUpdate(furnitureId, { $push: { likedList: userId } })
-};
+// exports.like = async (userId, furnitureId) => {
+//     await Furniture.findByIdAndUpdate(furnitureId, { $push: { likedList: userId } })
+// };
 
-exports.edit = (furnitureId, furnitureData) => Furniture.findByIdAndUpdate(furnitureId, furnitureData);
+exports.edit = (furnitureId, furnitureData) =>
+  Furniture.findByIdAndUpdate(furnitureId, furnitureData);
 
 exports.delete = (furnitureId) => Furniture.findByIdAndDelete(furnitureId);
 
-exports.search = async(name) => {
+// exports.search = async (name) => {
 
-    const partialString = name;
+//     const partialString = name;
+// console.log(partialString);
+//     const nameRegex = new RegExp(partialString, 'i');
+//     const categoryRegex = new RegExp(partialString, 'i');
 
-    return await Furniture.find({ name: new RegExp(partialString, 'i') }).lean();
+//     Furniture.find({ name: { $regex: nameRegex }, category: { $regex: categoryRegex } }, (err, furniture) => {
+//         if (err) {
+//             console.error(err);
+//         } else {
+// console.log(furniture);
+//             return furniture;
+//         }
+//     });
+
+//     // return await Furniture.find({ name: new RegExp(partialString, 'i') }).lean();
+// };
+
+exports.search = async (name) => {
+  const partialString = name;
+  const nameRegex = new RegExp(partialString, "i");
+  const categoryRegex = new RegExp(partialString, "i");
+
+  try {
+    const furniture = await Furniture.find({
+      $or: [
+        { name: { $regex: nameRegex } },
+        { category: { $regex: categoryRegex } },
+      ],
+    }).lean();
+
+    return furniture;
+  }
+  
+  catch (err) {
+    console.error(err);
+    throw err;
+  }
 };
