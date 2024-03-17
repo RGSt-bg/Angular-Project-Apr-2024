273ed4e9cca48e3e79361495d62d4828d90cf048
Rumen Stefanov <rgst.bg@gmail.com>
2024-03-17 22:11:17 +0200

    Initializing Angular project

---
+++ furniture-shop/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [AppComponent, MainComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, CoreModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
